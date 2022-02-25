2c15df1b4c9aefd9392b390e62b745eb573a743f
renbiqian <844615849@qq.com>
2022-02-25 20:29:09 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/git/test.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author renbiqian
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("自动初始化git并且创建仓库");
+    }
+
+}
