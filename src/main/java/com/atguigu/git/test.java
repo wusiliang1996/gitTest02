3c19cee92c3c599c3637fabf3d0aee763ab06927
wusiliang1996 <63762074+wusiliang1996@users.noreply.github.com>
2022-02-25 20:35:58 +0800

    pull

--- src/main/java/com/atguigu/git/test.java
+++ src/main/java/com/atguigu/git/test.java
@@ -8,6 +8,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("自动初始化git并且创建仓库");
         System.out.println("pushTest");
+        System.out.println("pullTest");
     }
 
 }
